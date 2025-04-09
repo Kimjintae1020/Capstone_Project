c7415b21c4f4e3a7028974c01f7be08cda296484
김진태 <aasdxcz123@gmail.com>
2025-04-10 00:57:33 +0900

    inti: 개발환경 세팅

---
+++ src/test/java/JMP/JMP/JmpApplicationTests.java
@@ -0,0 +1,13 @@
+package JMP.JMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
